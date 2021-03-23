a0322529bf04d2ab16160a62275574e199a93c95
szuchunhuang <szu.schuang@gmail.com>
2021-03-23 17:55:51 +0800

    add fail

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -11,11 +11,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PriorityQueueTest {
     static Stream<Arguments> stringProvider() {
         return Stream.of(
-                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
-                Arguments.of(new int[]{7, 4, 1, 3}, new int[]{1, 3, 4, 7}),
-                Arguments.of(new int[]{8, 7, 9, 0, 6}, new int[]{0, 6, 7, 8, 9}),
-                Arguments.of(new int[]{-1, 10, 9, 2, -8}, new int[]{-8, -1, 2, 9, 10}),
-                Arguments.of(new int[]{-11, 7, 0, -3, 10}, new int[]{-11, -3, 0, 7, 10})
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{-2, 2, 4, 5}),
+                Arguments.of(new int[]{7, 4, 1, 3}, new int[]{-3, 1, 4, 7}),
+                Arguments.of(new int[]{8, 7, 9, 0, 6}, new int[]{-1, 6, 7, 8, 9}),
+                Arguments.of(new int[]{-1, 10, 9, 2, -8}, new int[]{-7, -1, 2, 9, 10}),
+                Arguments.of(new int[]{-11, 7, 0, -3, 10}, new int[]{-10, -3, 0, 7, 10})
         );
     }
 
